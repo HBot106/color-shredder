120662ffe275a84ed578009b99c3db8867460528
Henry Luengas <gyatso@pop-os.localdomain>
2019-10-22 22:34:43 -0700

    problems with the available list

--- colorShredder.py
+++ colorShredder.py
@@ -1,5 +1,6 @@
 import png
 import random
+import sys
 
 USE_AVERAGE = False
 COLOR_BIT_DEPTH = 8
@@ -14,6 +15,9 @@ class Coords:
     def __init__(self, x_coord, y_coord):
         self.x = x_coord
         self.y = y_coord
+
+    def __hash__(self):
+        return self.x ^ self.y
 
     # checks for equaltiy with another coordinate
     def isEqual(self, check_coords):
@@ -45,8 +49,7 @@ class Coords:
 
     # debug print
     def printCoords(self):
-        print("x: " + str(self.x))
-        print("y: " + str(self.y))
+        print("x: " + str(self.x) + ", y: " + str(self.y))
 
 
 class Color:
@@ -67,6 +70,13 @@ class Color:
     # return the rgb values as a list
     def getRawColor(self):
         return [self.r, self.g, self.b]
+
+    # checks for equaltiy with another color
+    def isEqual(self, check_color):
+        if ((self.r == check_color.r) and (self.g == check_color.g) and (self.b == check_color.b)):
+            return True
+        else:
+            return False
 
 
 class Canvas:
@@ -130,6 +140,28 @@ class Canvas:
         else:
             return min(neighborDifferences)
 
+    # return a list of adjacent coordinates bounded by the canvas size
+    def getValidNeighbors(self, target_coord):
+        neighbors = []
+
+        # loop over the 9 coordnates touching self (including self)
+        for i in range(3):
+            for j in range(3):
+
+                if (i == 1 and j == 1):
+                    # this pixel is self, it is not a neigbor, go to the next one
+                    continue
+
+                # calculate the neigbor's coordinates
+                neighbor = Coords(target_coord.x - 1 + i,
+                                  target_coord.y - 1 + j)
+
+                # if they are within the canvas add them to the final neigbor list
+                if (0 < neighbor.x < self.width) and (0 < neighbor.y < self.heigt):
+                    if (self.canvas[neighbor.y][neighbor.x].isEqual(Color(0, 0, 0))):
+                        neighbors.append(neighbor)
+        return neighbors
+
 
 def generateColors():
 
@@ -164,15 +196,68 @@ def main():
     allColors = generateColors()
     myCanvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     start = Coords(START_X, START_Y)
-    isAvailable = set()
+    isAvailable = []
+    count = 0
 
     # draw the first color at the starting pixel
-    myCanvas.setColorAt(allColors.pop(), start)
-    for neighbor in start.getNeighbors():
-        isAvailable.add(neighbor)
+    targetColor = allColors.pop()
+    myCanvas.setColorAt(targetColor, start)
+    count += 1
+    for neighbor in myCanvas.getValidNeighbors(start):
+        isAvailable.append(neighbor)
+
+    print("Pixels Colored: " + str(count))
+    print("Pixels Available: " + str(len(isAvailable)))
+    for available in isAvailable:
+        available.printCoords()
+    print("")
+
+    for i in range(10000):
+        targetColor = allColors.pop()
+        minCoord = Coords(0, 0)
+        minDistance = sys.maxsize
+
+        for available in isAvailable:
+            if not (myCanvas.getColorAt(available).isEqual(Color(0, 0, 0))):
+                print("This pixel shouldn't be available.")
+                print("Pixels Colored: " + str(count))
+                print("Pixels Available: " + str(len(isAvailable)))
+                for available in isAvailable:
+                    available.printCoords()
+                print("")
+                exit(1)
+
+            check = myCanvas.considerPixelAt(available, targetColor)
+            if (check < minDistance):
+                minDistance = check
+                minCoord = available
+
+        myCanvas.setColorAt(targetColor, minCoord)
+        count += 1
+        isAvailable.remove(minCoord)
+
+        for neighbor in myCanvas.getValidNeighbors(minCoord):
+            if (myCanvas.getColorAt(neighbor).isEqual(Color(0, 0, 0))):
+                # 
+                # DOESNT WORK
+                # 
+                if not (neighbor in isAvailable):
+                    isAvailable.append(neighbor)
+
+        if (i % 100 == 0):
+            print("Pixels Colored: " + str(count))
+            print("Pixels Available: " + str(len(isAvailable)))
+            for available in isAvailable:
+                available.printCoords()
+            print("")
+            # write the png file
+            myFile = open('fuck1.png', 'wb')
+            myWriter = png.Writer(CANVAS_WIDTH, CANVAS_HEIGHT, greyscale=False)
+            myWriter.write(myFile, myCanvas.toRawOutput())
+            myFile.close()
 
     # write the png file
-    myFile = open('fuck.png', 'wb')
+    myFile = open('fuck1.png', 'wb')
     myWriter = png.Writer(CANVAS_WIDTH, CANVAS_HEIGHT, greyscale=False)
     myWriter.write(myFile, myCanvas.toRawOutput())
     myFile.close()
