6c45c80b41795138df9cab96fadf88e06e33471b
Henry Luengas <gyatso@pop-os.localdomain>
2019-11-29 19:43:35 -0800

    unittesting, think i found the problem, but not the root cause yet

--- test-colorTools.py
+++ test-colorTools.py
@@ -6,77 +6,82 @@ import unittest
 
 import colorTools
 
+BLACK = numpy.array([0, 0, 0])
+RED = numpy.array([255, 0, 0])
+GREEN = numpy.array([0, 255, 0])
+BLUE = numpy.array([0, 0, 255])
+
 
 class TestColorTools(unittest.TestCase):
 
     def testGetColorDiff_blackBlack(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 0]), numpy.array([0, 0, 0]))//1, 0)
+            numpy.array(BLACK), numpy.array(BLACK))//1, 0)
 
     def testGetColorDiff_blackRed(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 0]), numpy.array([255, 0, 0]))//1, 255)
+            numpy.array(BLACK), numpy.array(RED))//1, 255)
 
     def testGetColorDiff_blackGreen(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 0]), numpy.array([0, 255, 0]))//1, 255)
+            numpy.array(BLACK), numpy.array(GREEN))//1, 255)
 
     def testGetColorDiff_blackBlue(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 0]), numpy.array([0, 0, 255]))//1, 255)
+            numpy.array(BLACK), numpy.array(BLUE))//1, 255)
 # =============================================================================
 
     def testGetColorDiff_redBlack(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([255, 0, 0]), numpy.array([0, 0, 0]))//1, 255)
+            numpy.array(RED), numpy.array(BLACK))//1, 255)
 
     def testGetColorDiff_redRed(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([255, 0, 0]), numpy.array([255, 0, 0]))//1, 0)
+            numpy.array(RED), numpy.array(RED))//1, 0)
 
     def testGetColorDiff_redGreen(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([255, 0, 0]), numpy.array([0, 255, 0]))//1, 360)
+            numpy.array(RED), numpy.array(GREEN))//1, 360)
 
     def testGetColorDiff_redBlue(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([255, 0, 0]), numpy.array([0, 0, 255]))//1, 360)
+            numpy.array(RED), numpy.array(BLUE))//1, 360)
 # =============================================================================
 
 
     def testGetColorDiff_greenBlack(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 255, 0]), numpy.array([0, 0, 0]))//1, 255)
+            numpy.array(GREEN), numpy.array(BLACK))//1, 255)
 
     def testGetColorDiff_greenRed(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 255, 0]), numpy.array([255, 0, 0]))//1, 360)
+            numpy.array(GREEN), numpy.array(RED))//1, 360)
 
     def testGetColorDiff_greenGreen(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 255, 0]), numpy.array([0, 255, 0]))//1, 0)
+            numpy.array(GREEN), numpy.array(GREEN))//1, 0)
 
     def testGetColorDiff_greenBlue(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 255, 0]), numpy.array([0, 0, 255]))//1, 360)
+            numpy.array(GREEN), numpy.array(BLUE))//1, 360)
 # =============================================================================
 
 
     def testGetColorDiff_blueBlack(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 255]), numpy.array([0, 0, 0]))//1, 255)
+            numpy.array(BLUE), numpy.array(BLACK))//1, 255)
 
     def testGetColorDiff_blueRed(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 255]), numpy.array([255, 0, 0]))//1, 360)
+            numpy.array(BLUE), numpy.array(RED))//1, 360)
 
     def testGetColorDiff_blueGreen(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 255]), numpy.array([0, 255, 0]))//1, 360)
+            numpy.array(BLUE), numpy.array(GREEN))//1, 360)
 
     def testGetColorDiff_blueBlue(self):
         self.assertEqual(colorTools.getColorDiff(
-            numpy.array([0, 0, 255]), numpy.array([0, 0, 255]))//1, 0)
+            numpy.array(BLUE), numpy.array(BLUE))//1, 0)
 
 
 if __name__ == '__main__':
